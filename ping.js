af82cddba156eb1562188913e09d92d1f8a63fed
Matthew Hatcher <MatthewSH@users.noreply.github.com>
2018-06-13 05:30:25 -0400

    Adding core code.

--- ping.js
+++ ping.js
@@ -1,1 +1,27 @@
-//
+exports.commands = ["ping"];
+
+let api;
+let helper; 
+let messageNumber;
+
+exports.constructor = (api, helper) => {
+    this.messageNumber = 0;
+    this.api = api;
+    this.helper = helper;
+};
+
+exports.onChatMessage = {
+    execute: (data, IChatMessage) => {
+        this.messageNumber++;
+    }
+}
+
+exports.ping = {
+    description: "Ping!",
+    execute: (command, parameters, message) => {
+        if(this.messageNumber >= 5) {
+            this.api.say("Pong!");
+            this.messageNumber = 0;
+        }
+    }
+};
